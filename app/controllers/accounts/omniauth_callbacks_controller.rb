7a3ecf513cf89c01f40be3c41f6860a1a29c9b44
User Name <buitiendo@github.com>
2019-06-28 12:57:50 +0700

    login instagram

--- app/controllers/accounts/omniauth_callbacks_controller.rb
+++ app/controllers/accounts/omniauth_callbacks_controller.rb
@@ -9,4 +9,15 @@ class Accounts::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
       redirect_to new_account_registration_url, alert: @account.errors.full_messages.join("\n")
     end
   end
+
+  def instagram
+      @account = Account.from_omniauth request.env["omniauth.auth"]
+      if @account.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
+      sign_in_and_redirect @account, event: :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"].except :extra
+      redirect_to new_account_registration_url, alert: @account.errors.full_messages.join("\n")
+    end
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -2,11 +2,12 @@ class Account < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :omniauthable, :omniauth_providers => [:google_oauth2]
-
+         :recoverable, :rememberable, :omniauthable, :omniauth_providers => [:google_oauth2, :instagram]
+         
   def self.from_omniauth auth
     where(provider: auth.provider, uid: auth.uid).first_or_create do |account|
-      account.email = auth.info.email
+      # account.email = auth.info.nickname # đăng nhập với instagram thì comment dòng bên dưới lại.
+      account.email = auth.info.email # đăng nhập với google
       account.password = Devise.friendly_token[0,20]
       account.password_confirmation = Devise.friendly_token[0,20]
     end
