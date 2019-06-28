0bbc582b2b0defba7c158b3287689c48b80b6be8
User Name <buitiendo@github.com>
2019-06-28 12:57:50 +0700

    login instagram

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,4 +13,5 @@ Devise.setup do |config|
   config.sign_out_via = :delete
   config.scoped_views = true
   config.omniauth :google_oauth2,  "527195347193-6tb2fbte77qouu1tlpfdc876qq4ujsnj.apps.googleusercontent.com", "hW2hef4qSUDTrgleZ8tI7vWW", {}
+  config.omniauth :instagram, "16ab60d810ab4ef0be27f7746de0074a", "f8112dfaec0d4fc9af9d1ccbe6c75dae", token_params: { parse: :json }
 end
